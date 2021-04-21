cf728267136fd0fc4874d3dc69ceb934b946884c
Kavya Madhu <38519531+sigma-kmadhu@users.noreply.github.com>
2021-04-21 17:51:57 +0530

    testing error message

--- test/validation.js
+++ test/validation.js
@@ -3,7 +3,7 @@ $(function () {
 
     $registerInputs.on('invalid', function () {
         if ($(this).get(0).validity.valueMissing) {
-            $(this).get(0).setCustomValidity('Please fill out this field.');
+            $(this).get(0).setCustomValidity('Please fillll out this field.');
         } else {
             $(this).get(0).setCustomValidity('Please enter no more than 128 characters.');
         }
