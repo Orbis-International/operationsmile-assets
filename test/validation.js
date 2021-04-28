f3515d9c225c1b10995ae604193d6ecaa12008d5
kavyahsgowda <kavya.hs@sigmainfo.net>
2021-04-28 15:54:26 +0530

    updated validation.js

--- test/validation.js
+++ test/validation.js
@@ -1,15 +1,17 @@
-setTimeout($(function () {
-    var $registerInputs = $('#register input[type=text]');
+$(function () {
+    setTimeout(function(){
+        var $registerInputs = $('#register input[type=text]');
 
-    $registerInputs.on('invalid', function () {
-        if ($(this).get(0).validity.valueMissing) {
-            $(this).get(0).setCustomValidity('Please fillll out this field.');
-        } else {
-            $(this).get(0).setCustomValidity('Please enter no more than 128 characters.');
-        }
-    });
+        $registerInputs.on('invalid', function () {
+            if ($(this).get(0).validity.valueMissing) {
+                $(this).get(0).setCustomValidity('Please fillll out this field.');
+            } else {
+                $(this).get(0).setCustomValidity('Please enter no more than 128 characters.');
+            }
+        });
 
-    $registerInputs.on('input, change', function () {
-        $(this).get(0).setCustomValidity('');
-    });
-}), 3000);
+        $registerInputs.on('input, change', function () {
+            $(this).get(0).setCustomValidity('');
+        });
+    }, 5000);
+});
