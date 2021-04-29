91dd5cc2ee9e8ad447b932ca9b51f67b6678b977
sivavishnu_k <sivavishnu.k@sigmainfo.net>
2021-04-29 12:09:09 +0530

    localization

--- test/validation.js
+++ test/validation.js
@@ -1,9 +1,21 @@
 $(window).on('load', function () {
     console.log("Window Loaded");
     setTimeout(function(){
-        $.getJSON('localization.json', function(data) {
-            console.log(data);
-          });
+        function loadDoc() {
+            console.log("load doc")
+            var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+              if (this.readyState == 4 && this.status == 200) {
+               console.log(this.responseText);
+              }else{
+                console.log("nope")
+              }
+            };
+            xhttp.open("GET", "ajax_info.txt", true);
+            xhttp.send();
+        }
+        
+        loadDoc();
         console.log("*******Validation error messages**********");
         var $registerInputs = $('#register input[type=text]');
         $registerInputs.on('invalid', function () {
