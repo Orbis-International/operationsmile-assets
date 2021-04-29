3432c4db0cc03b57568c18e1e6f56df7101b9d67
sivavishnu_k <sivavishnu.k@sigmainfo.net>
2021-04-29 16:52:17 +0530

    validation

--- test/register.js
+++ test/register.js
@@ -1,6 +1,25 @@
 var emailField = document.getElementById('email-address')
     , confirmEmailField = document.getElementById('confirm-email-address');
 
+var error_category = getErrorCategories();
+var validation_json = localization_json();
+var lang = new URLSearchParams(window.location.search).get('lang')
+
+var $registerInputs = $('#register input[type=text], #register select');
+    $registerInputs.on('invalid', function () {
+        if ($(this).get(0).validity.valueMissing) {
+            $(this).get(0).setCustomValidity(validation_message("valueMissing"));
+        } else {
+            $(this).get(0).setCustomValidity(validation_message("value128Characters"));
+        }
+    });
+
+$registerInputs.on('input, change', function () {
+    $(this).get(0).setCustomValidity('');
+});
+function validation_message(key){
+    return validation_json[lang][error_category[key]]
+}
 function validateEmail(){
     if(emailField.value.trim() != confirmEmailField.value.trim()) {
         confirmEmailField.setCustomValidity(validation_message("confirmEmail"));
