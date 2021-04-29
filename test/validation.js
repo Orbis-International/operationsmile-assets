7c3966840d7f98b785b74773353c4f087bc378f9
sivavishnu_k <sivavishnu.k@sigmainfo.net>
2021-04-29 15:55:23 +0530

    validation

--- test/validation.js
+++ test/validation.js
@@ -10,23 +10,32 @@ $(window).on('load', function () {
         lang = lang == null ? "en" : lang
         var $registerInputs = $('#register input[type=text], #register select');
         $registerInputs.on('invalid', function () {
+            console.log.("...invalid")
+            console.log($(this))
+            console.log($(this).get(0).validity)
             if ($(this).get(0).validity.valueMissing) {
+                console.log("value missing...")
                 $(this).get(0).setCustomValidity(validation_json[lang][error_category["valueMissing"]]);
             } else if ($(this).get(0).validity.patternMismatch) {
+                console.log("pattern ...")
                 $(this).get(0).setCustomValidity(validation_json[lang][error_category["value128Characters"]]);
             } else if ($(this).get(0).validity.typeMismatch) {
-                $(this).get(0).setCustomValidity(validation_json[lang][error_category["invalidEmail"]]);
+                console.log("type.....")
+                $(this).get(0).setCustomValidity(validation_json[lang][error_category["value128Characters"]]);
             }
         });
-        
         $registerInputs.on('input, change', function () {
+            console.log("input change.....")
             $(this).get(0).setCustomValidity('');
         });
 
         function validateEmail(){
+            console.log("balidate email......")
             if(emailField.value.trim() != confirmEmailField.value.trim()) {
+                console.log("if confirm email......")
                 confirmEmailField.setCustomValidity(validation_json[lang][error_category["confirmEmail"]]);
             } else {
+                console.log("else confirm  email......")
                 confirmEmailField.setCustomValidity('');
             }
 
@@ -43,7 +52,7 @@ $(window).on('load', function () {
             // }
         }
 
-        // emailField.onchange = validateEmail();
-        // confirmEmailField.oninput = validateEmail();
+        emailField.onchange = validateEmail();
+        confirmEmailField.oninput = validateEmail();
     }, 2000);    
 });
