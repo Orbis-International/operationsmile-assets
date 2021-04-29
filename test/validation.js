5ba17f1864cd77777bfd51a1afb2f065cdc0445c
sivavishnu_k <sivavishnu.k@sigmainfo.net>
2021-04-29 16:45:40 +0530

    Deployment 1619694939:test

--- test/validation.js
+++ test/validation.js
@@ -1,26 +1,24 @@
-$(window).on('load', function () {
-    console.log("Window Loaded");
-    var error_category = getErrorCategories();
-    var validation_json = localization_json();
-    var lang = new URLSearchParams(window.location.search).get('lang')
-    setTimeout(function(){
-        if (lang != null){
-            var $registerInputs = $('#register input[type=text], #register select');
-            $registerInputs.on('invalid', function () {
-                if ($(this).get(0).validity.valueMissing) {
-                    $(this).get(0).setCustomValidity(validation_message("valueMissing"));
-                } else {
-                    $(this).get(0).setCustomValidity(validation_message("value128Characters"));
-                }
-            });
+console.log("Window Loaded");
+var error_category = getErrorCategories();
+var validation_json = localization_json();
+var lang = new URLSearchParams(window.location.search).get('lang')
+setTimeout(function(){
+    if (lang != null){
+        var $registerInputs = $('#register input[type=text], #register select');
+        $registerInputs.on('invalid', function () {
+            if ($(this).get(0).validity.valueMissing) {
+                $(this).get(0).setCustomValidity(validation_message("valueMissing"));
+            } else {
+                $(this).get(0).setCustomValidity(validation_message("value128Characters"));
+            }
+        });
 
-            $registerInputs.on('input, change', function () {
-                $(this).get(0).setCustomValidity('');
-            });
-        }
-    }, 5000);
-
-    function validation_message(key){
-        return validation_json[lang][error_category[key]]
+        $registerInputs.on('input, change', function () {
+            $(this).get(0).setCustomValidity('');
+        });
     }
-});
+}, 5000);
+
+function validation_message(key){
+    return validation_json[lang][error_category[key]]
+}
