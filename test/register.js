d0aa50a96aa770bfdf4e2331c2fde3a47be3de4e
sivavishnu_k <sivavishnu.k@sigmainfo.net>
2021-04-29 17:09:33 +0530

    validation

--- test/register.js
+++ test/register.js
@@ -1,56 +1,11 @@
 $(window).on('load', function () {
-    setTimeout(function(){
-        var emailField = document.getElementById('email-address')
-            , confirmEmailField = document.getElementById('confirm-email-address');
-
-        var error_category = getErrorCategories();
-        var validation_json = localization_json();
-        var lang = new URLSearchParams(window.location.search).get('lang')
-
-        var $registerInputs = $('#register input[type=text],#register input[type=email], #register select');
-            $registerInputs.on('invalid', function () {
-                if ($(this).get(0).validity.valueMissing) {
-                    $(this).get(0).setCustomValidity(validation_message("valueMissing"));
-                } else if ($(this).get(0).validity.patternMismatch){
-                    $(this).get(0).setCustomValidity(validation_message("value128Characters"));
-                }else if ($(this).get(0).validity.typeMismatch){
-                    $(this).get(0).setCustomValidity(validation_message("invalidEmail"));
-                }
-            });
-
-        $registerInputs.on('input, change', function () {
-            $(this).get(0).setCustomValidity('');
-        });
-        function validation_message(key){
-            return validation_json[lang][error_category[key]]
-        }
-        function validateEmail(){
-            if(emailField.value.trim() != confirmEmailField.value.trim()) {
-                confirmEmailField.setCustomValidity(validation_message("confirmEmail"));
-            } else {
-                confirmEmailField.setCustomValidity('');
-            }
-
-            // if(	emailField.validity.typeMismatch ) {
-            //     emailField.setCustomValidity(validation_message("invalidEmail"));
-            // } else {
-            //     emailField.setCustomValidity('');
-            // }
-
-            // if( emailField.validity.patternMismatch ) {
-            //     emailField.setCustomValidity(validation_message("value128Characters"));
-            // } else {
-            //     emailField.setCustomValidity('');
-            // }
-        }
-
-        emailField.onchange = validateEmail;
-        confirmEmailField.onchange = validateEmail;
-    }, 5000);
+    var errorMessage = ""
+    init();
 });
-$(function () {
 
-    var errorMessage = "Please enter no more than 60000 characters.";
+function init() {
+
+    errorMessage = "Please enter no more than 60000 characters.";
     var hasError;
 
     $( document ).find( "textarea" ).on( "input change propertychange", function() {
--- test/validation.js
+++ test/validation.js
@@ -1,26 +1,50 @@
-// $(window).on('load', function () {
-//     console.log("Window Loaded");
-//     var error_category = getErrorCategories();
-//     var validation_json = localization_json();
-//     var lang = new URLSearchParams(window.location.search).get('lang')
-//     setTimeout(function(){
-//         if (lang != null){
-//             var $registerInputs = $('#register input[type=text], #register select');
-//             $registerInputs.on('invalid', function () {
-//                 if ($(this).get(0).validity.valueMissing) {
-//                     $(this).get(0).setCustomValidity(validation_message("valueMissing"));
-//                 } else {
-//                     $(this).get(0).setCustomValidity(validation_message("value128Characters"));
-//                 }
-//             });
+$(window).on('load', function () {
+    setTimeout(function(){
+        var emailField = document.getElementById('email-address')
+            , confirmEmailField = document.getElementById('confirm-email-address');
 
-//             $registerInputs.on('input, change', function () {
-//                 $(this).get(0).setCustomValidity('');
-//             });
-//         }
-//     }, 5000);
+        var error_category = getErrorCategories();
+        var validation_json = localization_json();
+        var lang = new URLSearchParams(window.location.search).get('lang')
 
-//     function validation_message(key){
-//         return validation_json[lang][error_category[key]]
-//     }
-// });
+        var $registerInputs = $('#register input[type=text],#register input[type=email], #register select');
+            $registerInputs.on('invalid', function () {
+                if ($(this).get(0).validity.valueMissing) {
+                    $(this).get(0).setCustomValidity(validation_message("valueMissing"));
+                } else if ($(this).get(0).validity.patternMismatch){
+                    $(this).get(0).setCustomValidity(validation_message("value128Characters"));
+                }else if ($(this).get(0).validity.typeMismatch){
+                    $(this).get(0).setCustomValidity(validation_message("invalidEmail"));
+                }
+            });
+
+        $registerInputs.on('input, change', function () {
+            $(this).get(0).setCustomValidity('');
+        });
+        function validation_message(key){
+            return validation_json[lang][error_category[key]]
+        }
+        function validateEmail(){
+            if(emailField.value.trim() != confirmEmailField.value.trim()) {
+                confirmEmailField.setCustomValidity(validation_message("confirmEmail"));
+            } else {
+                confirmEmailField.setCustomValidity('');
+            }
+
+            // if(	emailField.validity.typeMismatch ) {
+            //     emailField.setCustomValidity(validation_message("invalidEmail"));
+            // } else {
+            //     emailField.setCustomValidity('');
+            // }
+
+            // if( emailField.validity.patternMismatch ) {
+            //     emailField.setCustomValidity(validation_message("value128Characters"));
+            // } else {
+            //     emailField.setCustomValidity('');
+            // }
+        }
+
+        emailField.onchange = validateEmail;
+        confirmEmailField.onchange = validateEmail;
+    }, 3000);
+});
