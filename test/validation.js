04870f6f1c22a9b892aae650f4e01c7bb8738742
kavyahsgowda <kavya.hs@sigmainfo.net>
2021-05-10 18:26:46 +0530

    email validation updated

--- test/validation.js
+++ test/validation.js
@@ -7,7 +7,7 @@ $(window).on('load', function () {
         var validation_json = localization_json();
         var lang = new URLSearchParams(window.location.search).get('lang')
 
-        var $registerInputs = $('#register input[type=text],#register input[type=email], #register select');
+        var $registerInputs = $('#register input[type=text], #register select');
             $registerInputs.on('invalid', function () {
                 if ($(this).get(0).validity.valueMissing) {
                     $(this).get(0).setCustomValidity(validation_message("valueMissing"));
@@ -33,17 +33,17 @@ $(window).on('load', function () {
                 confirmEmailField.setCustomValidity('');
             }
 
-            // if(	emailField.validity.typeMismatch ) {
-            //     emailField.setCustomValidity(validation_message("invalidEmail"));
-            // } else {
-            //     emailField.setCustomValidity('');
-            // }
+            if(emailField.validity.valueMissing ) {
+                emailField.setCustomValidity(validation_message("valueMissing"));
+            } else {
+                emailField.setCustomValidity('');
+            }
 
-            // if( emailField.validity.patternMismatch ) {
-            //     emailField.setCustomValidity(validation_message("value128Characters"));
-            // } else {
-            //     emailField.setCustomValidity('');
-            // }
+            if( emailField.validity.patternMismatch ) {
+                emailField.setCustomValidity(validation_message("invalidEmail"));
+            } else {
+                emailField.setCustomValidity('');
+            }
         }
 
         emailField.onchange = validateEmail;
